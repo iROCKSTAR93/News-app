8cd23c79ac90212990e2849c4d5f72fbe0542f2a
Naveen Chaudhary <98143534+VENOMnvn@users.noreply.github.com>
2023-12-24 01:40:04 +0530

    Update Navbar.js

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -5,7 +5,7 @@ function Navbar(props) {
         <div>
             <nav className="navbar navbar-expand-lg bg-dark" data-bs-theme="dark" >
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#"><span className="badge fs-4">The Daddy</span></a>
+                    <a className="navbar-brand" href="#"><span className="badge fs-4">The Daddy version 2</span></a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
